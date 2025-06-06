a554e14b4f1537f103e130d615568c30c4845ef6
Sokhna Gueye <sokhnag98@gmail.com>
2025-06-06 16:25:03 +0200

    feat(footer):add the html and the logo

--- annuaire/src/app/app.ts
+++ annuaire/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarreDeRecherche } from './barre-de-recherche/barre-de-recherche';
+import { Footer } from './features/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BarreDeRecherche],
+  imports: [RouterOutlet, BarreDeRecherche, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
