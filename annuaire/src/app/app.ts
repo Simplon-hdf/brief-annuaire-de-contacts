fe883d732b21ff11006867b7616e36e5437804be
Nicolas <lafuentenicolas1@gmail.com>
2025-06-06 14:34:05 +0200

    feat(app): add link for the card-customer

--- annuaire/src/app/app.ts
+++ annuaire/src/app/app.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarreDeRecherche } from './barre-de-recherche/barre-de-recherche';
 import { Header } from './header/header';
-
+import { CardClient } from './card-client/card-client';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BarreDeRecherche, Header],
+  imports: [RouterOutlet, BarreDeRecherche, Header, CardClient],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
