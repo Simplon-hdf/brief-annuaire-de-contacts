e5c59dfb948b89ae381e9a9ea69b9bb1525a5590
Marie KF <mariekfruchart@outlook.com>
2025-06-06 21:53:16 +0100

    fixed(contact.ts)fixed link between interface ModeleDeContact and class CardClient

--- annuaire/src/app/contact.ts
+++ annuaire/src/app/contact.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import {CardClient} from './card-client/card-client';
 export interface ModeleDeContact {
   id: number;
   nom: string;
@@ -18,11 +18,12 @@ export interface ModeleDeContact {
 })
 
 export class ContactService {
-  private CardClient = [
+  private CardClient: ModeleDeContact[] = [
     {
       id: 1,
       nom: 'Loic Wrobel',
       poste: 'Responsable Achats',
+      typeDeContact: 'Client',
       description: 'Client fidèle depuis 2021, intéressé par nos offres B2B.',
       email: 'lolo.wrowro@example.com',
       telephone: '06 12 34 56 78',
