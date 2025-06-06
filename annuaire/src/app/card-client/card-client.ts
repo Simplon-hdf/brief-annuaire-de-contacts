bb4447437c5636b050c571a97944d178ae75e8fc
Nicolas <lafuentenicolas1@gmail.com>
2025-06-06 14:33:29 +0200

    feat(header.html): add the ts for the first card customer

---
+++ annuaire/src/app/card-client/card-client.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-client',
+  imports: [],
+  templateUrl: './card-client.html',
+  styleUrl: './card-client.css'
+})
+export class CardClient {
+  nom: string = 'Loic Wrobel';
+  poste: string = 'Responsable Achats';
+  description: string = 'Client fidèle depuis 2021, intéressé par nos offres B2B.';
+  email: string = 'lolo.wrowro@example.com';
+  telephone: string = '06 12 34 56 78';
+  photoUrl: string = 'loic2.png'
+}
