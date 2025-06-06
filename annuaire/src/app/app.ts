ca243e1ab8619ac80607624f71398b0f3e83d041
Nicolas <lafuentenicolas1@gmail.com>
2025-06-06 11:50:25 +0200

    Feat(app):add links for the header conponent

--- annuaire/src/app/app.ts
+++ annuaire/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarreDeRecherche } from './barre-de-recherche/barre-de-recherche';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BarreDeRecherche],
+  imports: [RouterOutlet, BarreDeRecherche, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
