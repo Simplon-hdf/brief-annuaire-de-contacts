2c051a5a22c2655c0a23577a94f71d713202c7cb
Marie KF <mariekfruchart@outlook.com>
2025-06-06 22:53:15 +0100

    feat(formulaire-de-contact)updated the right properties for ngModel

---
+++ annuaire/src/app/formulaire-de-contact/formulaire-de-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-de-contact',
+  imports: [],
+  templateUrl: './formulaire-de-contact.html',
+  styleUrl: './formulaire-de-contact.css'
+})
+export class FormulaireDeContact {
+
+}
