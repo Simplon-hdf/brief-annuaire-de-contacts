d2ba860f3e8b54912e61c4b2df9e9fa36a0c1c4c
Marie KF <mariekfruchart@outlook.com>
2025-06-10 08:51:42 +0100

    feat(formulaire-de-contact.html)changed h2 headings to labels

--- annuaire/src/app/formulaire-de-contact/formulaire-de-contact.ts
+++ annuaire/src/app/formulaire-de-contact/formulaire-de-contact.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-
+import {FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-formulaire-de-contact',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulaire-de-contact.html',
   styleUrl: './formulaire-de-contact.css'
 })
 export class FormulaireDeContact {
 form: FormGroup = new FormGroup({
-  nom: new FormControl(''),
-  poste: new FormControl(''),
-  typeDeContact: new FormControl(''),
+  nom: new FormControl('', [Validators.required]),
+  poste: new FormControl('Client', [Validators.required]),
+  typeDeContact: new FormControl('', [Validators.required]),
   description: new FormControl(''),
-  email: new FormControl(''),
-  telephone: new FormControl(''),
-  photo: new FormControl('')
+  email: new FormControl('', [Validators.required]),
+  telephone: new FormControl('', [Validators.required]),
+  photoUrl: new FormControl('', [Validators.required])
 
 });
 
-onSubmit(){
+onSubmit(): void{
 
 }
 }
