b4636a4b35be7ec8621ebb352008cceeceab3296
Marie KF <mariekfruchart@outlook.com>
2025-06-10 06:31:21 +0100

    feat(formulaire-de-contact)cleaned code to avoid merge conflicts

--- annuaire/src/app/app.ts
+++ annuaire/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarreDeRecherche } from './barre-de-recherche/barre-de-recherche';
 import { Header } from './header/header';
+import { FormulaireDeContact } from './formulaire-de-contact/formulaire-de-contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BarreDeRecherche, Header],
+  imports: [RouterOutlet, BarreDeRecherche, Header, FormulaireDeContact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
