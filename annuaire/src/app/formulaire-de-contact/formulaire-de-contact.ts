cb48b609900bc426189bdf456a500d3b6e3bc413
Marie KF <mariekfruchart@outlook.com>
2025-06-10 07:20:38 +0100

    feat(formulaire-de-contact.ts)added elements to use a reactive form module

--- annuaire/src/app/formulaire-de-contact/formulaire-de-contact.ts
+++ annuaire/src/app/formulaire-de-contact/formulaire-de-contact.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-de-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulaire-de-contact.html',
   styleUrl: './formulaire-de-contact.css'
 })
 export class FormulaireDeContact {
+form: FormGroup = new FormGroup({
+  nom: new FormControl(''),
+  poste: new FormControl(''),
+  typeDeContact: new FormControl(''),
+  description: new FormControl(''),
+  email: new FormControl(''),
+  telephone: new FormControl(''),
+  photo: new FormControl('')
 
+});
+
+onSubmit(){
+
+}
 }
