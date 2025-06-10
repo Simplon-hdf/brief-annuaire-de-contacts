c01a3f8a32b187ca8338d22a07a5416d5f8f5804
Nicolas <lafuentenicolas1@gmail.com>
2025-06-10 14:18:59 +0200

    feat(public): add home page and new-page

---
+++ annuaire/src/app/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { GeorgeC } from '../../george-c/george-c';
+import { CardClient } from '../../card-client/card-client';
+import { CarteFournisseurs } from '../../carte-fournisseurs/carte-fournisseurs';
+import { BarreDeRecherche } from '../../barre-de-recherche/barre-de-recherche';
+import { JeanC } from '../../jean-c/jean-c';
+import { RobertC } from '../../robert-c/robert-c';
+import { LolaC } from '../../lola-c/lola-c';
+import { ClaraC } from '../../clara-c/clara-c';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [GeorgeC,CardClient, CarteFournisseurs, BarreDeRecherche, JeanC, RobertC,LolaC,ClaraC ],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
