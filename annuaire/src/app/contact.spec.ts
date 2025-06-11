99d86dfcd16fa51c39731383c0b329014b18e188
Nicolas <lafuentenicolas1@gmail.com>
2025-06-11 13:44:13 +0200

    fix(app):resolve conflict and error

--- annuaire/src/app/contact.spec.ts
+++ annuaire/src/app/contact.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { Contact } from '../contact'; src/app/contact.ts
+import { Contact } from './contact'; 
 
 describe('Contact', () => {
   let service: Contact;
